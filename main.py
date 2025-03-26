31f37a8fca70a61dad0d0d7ea88694b4f0d6aaaa
nater1638 <nburke6@nd.edu>
2025-03-26 13:29:46 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 # EG10118 Section 12 Project 2 Program: Bomb Sniffing Robot
 # Nathan Burke, Edan Czarobski, Ben Muckian, Jack Whitman
 
+leftSpeed = 50
+rightSpeed = 50
 
-CutebotPro.full_speed_ahead()
+
+
+while True:
+    CutebotPro.pwm_cruise_control(leftSpeed, rightSpeed)
+
+    if CutebotPro.ultrasonic(SonarUnit.CENTIMETERS) < 10:
+
+        CutebotPro.stop_immediately(CutebotProMotors.M1)
+        music.play(music.tone_playable(Note.G, music.beat(BeatFraction.HALF)), music.PlaybackMode.UNTIL_DONE)
+        CutebotPro.trolley_steering(CutebotProTurn.LEFT, 180)
--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
 //  EG10118 Section 12 Project 2 Program: Bomb Sniffing Robot
 //  Nathan Burke, Edan Czarobski, Ben Muckian, Jack Whitman
-CutebotPro.fullSpeedAhead()
+let leftSpeed = 50
+let rightSpeed = 50
+while (true) {
+    CutebotPro.pwmCruiseControl(leftSpeed, rightSpeed)
+    if (CutebotPro.ultrasonic(SonarUnit.Centimeters) < 10) {
+        CutebotPro.stopImmediately(CutebotProMotors.M1)
+        music.play(music.tonePlayable(Note.G, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+        CutebotPro.trolleySteering(CutebotProTurn.Left, 180)
+    }
+    
+}
