fbbe4440b83dc86d9452c7a890249c332eb3518a
nater1638 <nburke6@nd.edu>
2025-03-26 13:45:01 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 # EG10118 Section 12 Project 2 Program: Bomb Sniffing Robot
 # Nathan Burke, Edan Czarobski, Ben Muckian, Jack Whitman
 
-leftSpeed = 50
-rightSpeed = 50
+leftSpeed = 30
+rightSpeed = 30
 
 
 
 while True:
+    CutebotPro.color_light(CutebotProRGBLight.RGBL, 0xffffff)
     CutebotPro.pwm_cruise_control(leftSpeed, rightSpeed)
-
-    if CutebotPro.ultrasonic(SonarUnit.CENTIMETERS) < 10:
-
-        CutebotPro.stop_immediately(CutebotProMotors.M1)
+    if CutebotPro.ultrasonic(SonarUnit.CENTIMETERS) < 20:
+        CutebotPro.color_light(CutebotProRGBLight.RGBL, 0xff00ff)
         music.play(music.tone_playable(Note.G, music.beat(BeatFraction.HALF)), music.PlaybackMode.UNTIL_DONE)
-        CutebotPro.trolley_steering(CutebotProTurn.LEFT, 180)
+        CutebotPro.stop_immediately(CutebotProMotors.M1)
+        CutebotPro.pwm_cruise_control(-30, 30)
+        control.wait_micros(450000)
--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 //  EG10118 Section 12 Project 2 Program: Bomb Sniffing Robot
 //  Nathan Burke, Edan Czarobski, Ben Muckian, Jack Whitman
-let leftSpeed = 50
-let rightSpeed = 50
+let leftSpeed = 30
+let rightSpeed = 30
 while (true) {
+    CutebotPro.colorLight(CutebotProRGBLight.RGBL, 0xffffff)
     CutebotPro.pwmCruiseControl(leftSpeed, rightSpeed)
-    if (CutebotPro.ultrasonic(SonarUnit.Centimeters) < 10) {
-        CutebotPro.stopImmediately(CutebotProMotors.M1)
+    if (CutebotPro.ultrasonic(SonarUnit.Centimeters) < 20) {
+        CutebotPro.colorLight(CutebotProRGBLight.RGBL, 0xff00ff)
         music.play(music.tonePlayable(Note.G, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
-        CutebotPro.trolleySteering(CutebotProTurn.Left, 180)
+        CutebotPro.stopImmediately(CutebotProMotors.M1)
+        CutebotPro.pwmCruiseControl(-30, 30)
+        control.waitMicros(450000)
     }
     
 }
