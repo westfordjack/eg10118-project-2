87a539deb6e5564ecb9202ae5e1d314584d9f5f9
nater1638 <nburke6@nd.edu>
2025-03-31 13:47:20 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ leftSpeed = 30
 rightSpeed = 30
 
 
-
+'''
 while True:
     CutebotPro.color_light(CutebotProRGBLight.RGBL, 0xffffff)
     CutebotPro.pwm_cruise_control(leftSpeed, rightSpeed)
@@ -15,3 +15,20 @@ while True:
         CutebotPro.stop_immediately(CutebotProMotors.M1)
         CutebotPro.pwm_cruise_control(-30, 30)
         control.wait_micros(450000)
+'''
+
+baseline = input.magnetic_force(Dimension.X) # Take a baseline reading of magnetic strength
+print(baseline)
+control.wait_micros(10000000)
+
+while True:
+    value = input.magnetic_force(Dimension.X)
+    print(value)
+
+    if abs(value - baseline) > 30:
+        basic.show_icon(IconNames.NO)  
+          # Show a cross symbol
+    else:
+        basic.clear_screen()
+    
+    control.wait_micros(50000)
--- main.ts
+++ main.ts
@@ -1,16 +1,33 @@
+let value: number;
 //  EG10118 Section 12 Project 2 Program: Bomb Sniffing Robot
 //  Nathan Burke, Edan Czarobski, Ben Muckian, Jack Whitman
 let leftSpeed = 30
 let rightSpeed = 30
+/** 
+while True:
+    CutebotPro.color_light(CutebotProRGBLight.RGBL, 0xffffff)
+    CutebotPro.pwm_cruise_control(leftSpeed, rightSpeed)
+    if CutebotPro.ultrasonic(SonarUnit.CENTIMETERS) < 20:
+        CutebotPro.color_light(CutebotProRGBLight.RGBL, 0xff00ff)
+        music.play(music.tone_playable(Note.G, music.beat(BeatFraction.HALF)), music.PlaybackMode.UNTIL_DONE)
+        CutebotPro.stop_immediately(CutebotProMotors.M1)
+        CutebotPro.pwm_cruise_control(-30, 30)
+        control.wait_micros(450000)
+
+ */
+let baseline = input.magneticForce(Dimension.X)
+//  Take a baseline reading of magnetic strength
+console.log(baseline)
+control.waitMicros(10000000)
 while (true) {
-    CutebotPro.colorLight(CutebotProRGBLight.RGBL, 0xffffff)
-    CutebotPro.pwmCruiseControl(leftSpeed, rightSpeed)
-    if (CutebotPro.ultrasonic(SonarUnit.Centimeters) < 20) {
-        CutebotPro.colorLight(CutebotProRGBLight.RGBL, 0xff00ff)
-        music.play(music.tonePlayable(Note.G, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
-        CutebotPro.stopImmediately(CutebotProMotors.M1)
-        CutebotPro.pwmCruiseControl(-30, 30)
-        control.waitMicros(450000)
+    value = input.magneticForce(Dimension.X)
+    console.log(value)
+    if (Math.abs(value - baseline) > 30) {
+        basic.showIcon(IconNames.No)
+    } else {
+        //  Show a cross symbol
+        basic.clearScreen()
     }
     
+    control.waitMicros(50000)
 }
