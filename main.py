40988837318a24d4abc831e896f86256329dfb13
Jack Whitman <52720199+westfordjack@users.noreply.github.com>
2025-04-07 13:21:00 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,53 +1,159 @@
 # EG10118 Section 12 Project 2 Program: Bomb Sniffing Robot
 # Nathan Burke, Edan Czarobski, Ben Muckian, Jack Whitman
 
-leftSpeed = 20
-rightSpeed = 20
+straight_speed = 20 # Nominal speed of the robot
+left = True # Does the robot hug the left or right side of the line
+turn = 90 # This number should represent a right turn
 
+# Navigation parameters
+navigation = [(0,0)]
+direction = 0 # 0 = East, 1 = North, 2 = West, 3 = South
+x = navigation[0][0]
+y = navigation[0][1]
 
-'''
-while True:
-    CutebotPro.color_light(CutebotProRGBLight.RGBL, 0xffffff)
-    CutebotPro.pwm_cruise_control(leftSpeed, rightSpeed)
-    if CutebotPro.ultrasonic(SonarUnit.CENTIMETERS) < 20:
-        CutebotPro.color_light(CutebotProRGBLight.RGBL, 0xff00ff)
-        music.play(music.tone_playable(Note.G, music.beat(BeatFraction.HALF)), music.PlaybackMode.UNTIL_DONE)
-        CutebotPro.stop_immediately(CutebotProMotors.M1)
-        CutebotPro.pwm_cruise_control(-30, 30)
-        control.wait_micros(450000)
-'''
+# Setting block size and radio stuff
+CutebotPro.set_block_cnt(12, CutebotProDistanceUnits.FT)
+radio.set_group(1)
 
-baseline = abs(input.magnetic_force(Dimension.Y)) # Take a baseline reading of magnetic strength
-print(baseline)
-control.wait_micros(3000000)
+# Magnetic baseline and threshold
+baseline = abs(input.magnetic_force(Dimension.Y))
+magnetic_threshold = 50 #in microteslas
 
-CutebotPro.pwm_cruise_control(leftSpeed, rightSpeed)
+# Function to check if a magnet has been found
+def magnet_found():
+    return abs(input.magnetic_force(Dimension.Y)) - baseline > magnetic_threshold
 
-mainrun = True
+# Set the robot to go tangent to the current edge of the line
+def follow_line():
+    # Get distance from line (+3000 to -3000)
+    offset = CutebotPro.get_offset()
 
-while mainrun:
-    #continually takes magnetic vals
-    value = abs(input.magnetic_force(Dimension.Y))
-    print(value)
+    # If we want to follow the left side of the line, center ourselves at offset 1500
+    if left:
+        difference = offset - 1500
+        difference = difference if difference > -1500 else -1500
+    # If we want to follow the right side of the line, center ourselves at offset -1500
+    else:
+        difference = offset + 1500
+        difference = difference if difference < 1500 else 1500
 
-    #if magnet is detected
-    if abs(value - baseline) > 30:
-        print("True")
-        #display X
-        basic.show_icon(IconNames.NO)
-        
-        #stop bot
-        CutebotPro.stop_immediately(CutebotProMotors.ALL)
-        control.wait_micros(2000000)
-        
-        #initialize wheel rotation to zero
-        CutebotPro.clear_wheel_turn(CutebotProMotors1.M1)
+    # Difference ranges from +1500 to -1500, tells how far from target
 
-        #spin robot around
-        CutebotPro.trolley_steering(CutebotProTurn.LEFT_IN_PLACE, 180)
-        
-        CutebotPro.pwm_cruise_control(leftSpeed, rightSpeed)
-        control.wait_micros(100000)
+    # If we have the line at all, scale turning speed with distance from line
+    if abs(difference) != 1500:
+        turn_speed = straight_speed - (abs(difference) / 1500) * straight_speed
+    # If we lost the line, set turn speed to spin in a circle
+    else:
+        turn_speed = -straight_speed
 
+    # Turn left or right based on which side of target we are on
+    if difference > 0:
+        CutebotPro.pwm_cruise_control(straight_speed, turn_speed)
+    else:
+        CutebotPro.pwm_cruise_control(turn_speed, straight_speed)
 
-    basic.show_icon(IconNames.YES)
+# Turning functions
+def turn_right():
+    global direction
+    CutebotPro.trolley_steering(CutebotProTurn.RIGHT_IN_PLACE, turn)
+    direction = (direction - 1) % 4
+
+def turn_left():
+    global direction
+    CutebotPro.trolley_steering(CutebotProTurn.LEFT_IN_PLACE, turn)
+    direction = (direction + 1) % 4
+
+def turn_180():
+    global direction
+    CutebotPro.trolley_steering(CutebotProTurn.LEFT_IN_PLACE, turn * 2)
+    direction = (direction + 2) % 4
+
+# Checks for an obstacle
+def obstacle():
+    return CutebotPro.ultrasonic(SonarUnit.INCHES) < 6
+
+# Move forward, updating direction
+def forward():
+    global x
+    global y
+    global direction
+    if direction == 0:
+        x += 1
+    elif direction == 1:
+        y += 1
+    elif direction == 2:
+        x -= 1
+    else:
+        y -= 1
+    navigation.append((x, y))
+    CutebotPro.run_block_cnt(1)
+
+# Broadcast the solution to another bot
+def broadcast_solution():
+    pass
+
+# Navigate the maze
+def navigate_maze():
+    global x
+    global y
+    # Go until the magnet is found, following the left wall
+    while not magnet_found():
+        obs_forward = obstacle()
+        turn_left()
+        if not obstacle():
+            forward()
+        elif not obs_forward:
+            turn_right()
+            forward()
+        else:
+            turn_180()
+            if not obstacle():
+                forward()
+            else:
+                turn_right()
+                forward()
+
+# Head back to the start
+def navigate_back():
+    global navigation
+    global x
+    global y
+    global direction
+    num_steps = len(navigation)
+    for step_num in range(1, num_steps + 1):
+        next_step = navigation[num_steps - step_num]
+        dx = next_step[0] - x
+        dy = next_step[1] - y
+
+        if dx == 1:
+            goal_direction = 0
+        elif dy == 1:
+            goal_direction = 1
+        elif dx == -1:
+            goal_direction = 2
+        else:
+            goal_direction = 3
+
+        change_dir = (goal_direction - direction) % 4
+        if change_dir == 1:
+            turn_left()
+        elif change_dir == 2:
+            turn_180()
+        elif change_dir == 3:
+            turn_right()
+
+        forward()
+
+def main():
+    basic.show_number(1)
+    while not magnet_found():
+        follow_line()
+        control.wait_micros(1000)
+    basic.show_number(2)
+    navigate_maze()
+    basic.show_number(3)
+    broadcast_solution()
+    navigate_back()
+    basic.show_number(4)
+
+main()
--- main.ts
+++ main.ts
@@ -1,45 +1,190 @@
-let value: number;
 //  EG10118 Section 12 Project 2 Program: Bomb Sniffing Robot
 //  Nathan Burke, Edan Czarobski, Ben Muckian, Jack Whitman
-let leftSpeed = 20
-let rightSpeed = 20
-/** 
-while True:
-    CutebotPro.color_light(CutebotProRGBLight.RGBL, 0xffffff)
-    CutebotPro.pwm_cruise_control(leftSpeed, rightSpeed)
-    if CutebotPro.ultrasonic(SonarUnit.CENTIMETERS) < 20:
-        CutebotPro.color_light(CutebotProRGBLight.RGBL, 0xff00ff)
-        music.play(music.tone_playable(Note.G, music.beat(BeatFraction.HALF)), music.PlaybackMode.UNTIL_DONE)
-        CutebotPro.stop_immediately(CutebotProMotors.M1)
-        CutebotPro.pwm_cruise_control(-30, 30)
-        control.wait_micros(450000)
-
- */
+let straight_speed = 20
+//  Nominal speed of the robot
+let left = true
+//  Does the robot hug the left or right side of the line
+let turn = 90
+//  This number should represent a right turn
+//  Navigation parameters
+let navigation = [[0, 0]]
+let direction = 0
+//  0 = East, 1 = North, 2 = West, 3 = South
+let x = navigation[0][0]
+let y = navigation[0][1]
+//  Setting block size and radio stuff
+CutebotPro.setBlockCnt(12, CutebotProDistanceUnits.Ft)
+radio.setGroup(1)
+//  Magnetic baseline and threshold
 let baseline = Math.abs(input.magneticForce(Dimension.Y))
-//  Take a baseline reading of magnetic strength
-console.log(baseline)
-control.waitMicros(3000000)
-CutebotPro.pwmCruiseControl(leftSpeed, rightSpeed)
-let mainrun = true
-while (mainrun) {
-    // continually takes magnetic vals
-    value = Math.abs(input.magneticForce(Dimension.Y))
-    console.log(value)
-    // if magnet is detected
-    if (Math.abs(value - baseline) > 30) {
-        console.log("True")
-        // display X
-        basic.showIcon(IconNames.No)
-        // stop bot
-        CutebotPro.stopImmediately(CutebotProMotors.ALL)
-        control.waitMicros(2000000)
-        // initialize wheel rotation to zero
-        CutebotPro.clearWheelTurn(CutebotProMotors1.M1)
-        // spin robot around
-        CutebotPro.trolleySteering(CutebotProTurn.LeftInPlace, 180)
-        CutebotPro.pwmCruiseControl(leftSpeed, rightSpeed)
-        control.waitMicros(100000)
+let magnetic_threshold = 50
+// in microteslas
+//  Function to check if a magnet has been found
+function magnet_found() {
+    return Math.abs(input.magneticForce(Dimension.Y)) - baseline > magnetic_threshold
+}
+
+//  Set the robot to go tangent to the current edge of the line
+function follow_line() {
+    let difference: number;
+    let turn_speed: number;
+    //  Get distance from line (+3000 to -3000)
+    let offset = CutebotPro.getOffset()
+    //  If we want to follow the left side of the line, center ourselves at offset 1500
+    if (left) {
+        difference = offset - 1500
+        difference = difference > -1500 ? difference : -1500
+    } else {
+        //  If we want to follow the right side of the line, center ourselves at offset -1500
+        difference = offset + 1500
+        difference = difference < 1500 ? difference : 1500
     }
     
-    basic.showIcon(IconNames.Yes)
+    //  Difference ranges from +1500 to -1500, tells how far from target
+    //  If we have the line at all, scale turning speed with distance from line
+    if (Math.abs(difference) != 1500) {
+        turn_speed = straight_speed - Math.abs(difference) / 1500 * straight_speed
+    } else {
+        //  If we lost the line, set turn speed to spin in a circle
+        turn_speed = -straight_speed
+    }
+    
+    //  Turn left or right based on which side of target we are on
+    if (difference > 0) {
+        CutebotPro.pwmCruiseControl(straight_speed, turn_speed)
+    } else {
+        CutebotPro.pwmCruiseControl(turn_speed, straight_speed)
+    }
+    
 }
+
+//  Turning functions
+function turn_right() {
+    
+    CutebotPro.trolleySteering(CutebotProTurn.RightInPlace, turn)
+    direction = (direction - 1) % 4
+}
+
+function turn_left() {
+    
+    CutebotPro.trolleySteering(CutebotProTurn.LeftInPlace, turn)
+    direction = (direction + 1) % 4
+}
+
+function turn_180() {
+    
+    CutebotPro.trolleySteering(CutebotProTurn.LeftInPlace, turn * 2)
+    direction = (direction + 2) % 4
+}
+
+//  Checks for an obstacle
+function obstacle() {
+    return CutebotPro.ultrasonic(SonarUnit.Inches) < 6
+}
+
+//  Move forward, updating direction
+function forward() {
+    
+    
+    
+    if (direction == 0) {
+        x += 1
+    } else if (direction == 1) {
+        y += 1
+    } else if (direction == 2) {
+        x -= 1
+    } else {
+        y -= 1
+    }
+    
+    navigation.push([x, y])
+    CutebotPro.runBlockCnt(1)
+}
+
+//  Broadcast the solution to another bot
+function broadcast_solution() {
+    
+}
+
+//  Navigate the maze
+function navigate_maze() {
+    let obs_forward: any;
+    
+    
+    //  Go until the magnet is found, following the left wall
+    while (!magnet_found()) {
+        obs_forward = obstacle()
+        turn_left()
+        if (!obstacle()) {
+            forward()
+        } else if (!obs_forward) {
+            turn_right()
+            forward()
+        } else {
+            turn_180()
+            if (!obstacle()) {
+                forward()
+            } else {
+                turn_right()
+                forward()
+            }
+            
+        }
+        
+    }
+}
+
+//  Head back to the start
+function navigate_back() {
+    let next_step: number[];
+    let dx: number;
+    let dy: number;
+    let goal_direction: number;
+    let change_dir: any;
+    
+    
+    
+    
+    let num_steps = navigation.length
+    for (let step_num = 1; step_num < num_steps + 1; step_num++) {
+        next_step = navigation[num_steps - step_num]
+        dx = next_step[0] - x
+        dy = next_step[1] - y
+        if (dx == 1) {
+            goal_direction = 0
+        } else if (dy == 1) {
+            goal_direction = 1
+        } else if (dx == -1) {
+            goal_direction = 2
+        } else {
+            goal_direction = 3
+        }
+        
+        change_dir = (goal_direction - direction) % 4
+        if (change_dir == 1) {
+            turn_left()
+        } else if (change_dir == 2) {
+            turn_180()
+        } else if (change_dir == 3) {
+            turn_right()
+        }
+        
+        forward()
+    }
+}
+
+function main() {
+    basic.showNumber(1)
+    while (!magnet_found()) {
+        follow_line()
+        control.waitMicros(1000)
+    }
+    basic.showNumber(2)
+    navigate_maze()
+    basic.showNumber(3)
+    broadcast_solution()
+    navigate_back()
+    basic.showNumber(4)
+}
+
+main()
